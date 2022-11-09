80d58802b51e848a7fe057ed2f06cd0fe27c5fd5
KarlMarkus <karl-markus.vaher@voco.ee>
2022-11-09 16:14:17 +0300

    Display weather data from API

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const weather = new Weather('Tallinn')
 console.log(weather.getWeather())
 
+const ui = new UI()
+
 function getWeather() {
     weather.getWeather()
         .then(data => {
             console.log(data)
+            ui.drawWeather(data)
         })
         .catch(error => console.log(error))
 }
